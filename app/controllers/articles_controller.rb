8a3238386c0ae4d8dd133bdfe7ccf4919ce41dfa
valachi <impulse221@yandex.ru>
2011-03-06 12:51:56 +0300

    Added the new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,24 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end  
   
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to :action => "index"
+    else
+      render :new
+    end
   end
 
   def show
+    @article = Article.find(params[:article])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Science::Application.routes.draw do
+  resources :articles
+  
+  match ':controller(/:action(/:id(.:format)))'
   root :to => "articles#index"
 end
