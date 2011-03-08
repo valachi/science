b2baa4f04e3a9559640ecaea4bc14d5bdaa7c35d
valachi <impulse221@yandex.ru>
2011-03-08 21:38:10 +0300

    Added edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to @article
+    else
+      render :edit
+    end        
+  end
+
   def destroy
     Article.find(params[:id]).destroy
     redirect_to root_path
