9de06bdda368c587aee7bd6e508b8d168a4d3bde
valachi <impulse221@yandex.ru>
2011-03-12 14:18:27 +0300

    Done with flash massages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,13 @@
 class ArticlesController < InheritedResources::Base
+  def create
+    create!(:notice => "Your article was successfully created")
+  end
+
+  def update
+    update!(:notice => "Successfully updated")
+  end
+
+  def destroy
+    destroy!(:notice => "You have just destroyed an one very good article")
+  end
 end
