52a8e281afd0543cea2e069c04cf95370b5ef6a2
valachi <impulse221@yandex.ru>
2011-04-10 18:33:59 +0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :load_sidebar_data
   before_filter :ensure_domain
 
-  APP_DOMAIN = "scione.ru/"
+  APP_DOMAIN = "scione.ru"
 
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -23,9 +23,9 @@ class ApplicationController < ActionController::Base
   end
 
   def ensure_domain
-    if request.env['HTTP_HOST'] != APP_DOMAIN
+    if request.env['HTTP_HOST'] != "#{APP_DOMAIN}/#{params[:path]}"
       # HTTP 301 is a "permanent" redirect
-      redirect_to "http://#{APP_DOMAIN}#{params[:path]}", :status => 301
+      redirect_to "http://#{APP_DOMAIN}/#{params[:path]}", :status => 301
     end
   end
 end
